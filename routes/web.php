1b89cf046015f886c4e2ea5f14635582d67515ee
Will Verland <will@ver.land>
2023-04-30 11:12:42 +1200

    Eager load user agents with requests

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', function () {
-    return view('index', ['requests' => AppRequest::all()]);
+    return view('index', ['requests' => AppRequest::with('userAgent')->get()]);
 });
