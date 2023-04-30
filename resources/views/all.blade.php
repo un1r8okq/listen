58552e915d6ca330e3577f8eafda7231e8ffd396
Will Verland <will@ver.land>
2023-04-30 17:27:48 +1200

    Make tables repsponsive

--- resources/views/all.blade.php
+++ resources/views/all.blade.php
@@ -1,24 +1,26 @@
 <x-layout>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Time</th>
-                <th scope="col">IP Address</th>
-                <th scope="col">User Agent</th>
-                <th scope="col">URL</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($requests as $request)
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
                 <tr>
-                    <th scope="row">{{ $request->id }}</th>
-                    <td>{{ $request->created_at }}</td>
-                    <td>{{ $request->ip_address }}</td>
-                    <td>{{ $request->userAgent->user_agent }}</td>
-                    <td>{{ $request->url }}</td>
+                    <th scope="col">#</th>
+                    <th scope="col">Time</th>
+                    <th scope="col">IP Address</th>
+                    <th scope="col">User Agent</th>
+                    <th scope="col">URL</th>
                 </tr>
-            @endforeach
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($requests as $request)
+                    <tr>
+                        <th scope="row">{{ $request->id }}</th>
+                        <td>{{ $request->created_at }}</td>
+                        <td>{{ $request->ip_address }}</td>
+                        <td>{{ $request->userAgent->user_agent }}</td>
+                        <td>{{ $request->url }}</td>
+                    </tr>
+                @endforeach
+        </table>
+    </div>
     {{ $requests->links() }}
 </x-layout>
--- resources/views/by-url.blade.php
+++ resources/views/by-url.blade.php
@@ -1,18 +1,20 @@
 <x-layout>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">URL</th>
-                <th scope="col">Number of requests</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($urls as $url)
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
                 <tr>
-                    <th scope="row">{{ $url->url }}</th>
-                    <td>{{ $url->num_requests }}</td>
+                    <th scope="col">URL</th>
+                    <th scope="col">Number of requests</th>
                 </tr>
-            @endforeach
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($urls as $url)
+                    <tr>
+                        <th scope="row">{{ $url->url }}</th>
+                        <td>{{ $url->num_requests }}</td>
+                    </tr>
+                @endforeach
+        </table>
+    </div>
     {{ $urls->links() }}
 </x-layout>
