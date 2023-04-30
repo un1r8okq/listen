a866023ac7b8de5a2a5d09ac78c4758e5133e961
Will Verland <will@ver.land>
2023-04-30 17:25:56 +1200

    Add Bootstrap JS for nav on mobile

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,9 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>{{ config('app.name') }}</title>
 
+        <!-- Bootstrap CSS used for collapsible nav on mobile -->
         <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
             rel="stylesheet"
@@ -17,7 +17,15 @@
         <nav class="navbar navbar-expand-lg bg-light mb-4">
             <div class="container-fluid">
                 <a class="navbar-brand" href="/">Listen</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavDropdown">
@@ -65,5 +73,11 @@
         <div class="container">
             {{ $slot }}
         </div>
+        <!-- Bootstrap JS used for collapsible nav on mobile -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+            crossorigin="anonymous"
+        ></script>
     </body>
 </html>
